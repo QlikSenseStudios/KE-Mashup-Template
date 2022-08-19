a0f7bb34c2446f9732ce4b988f26d39713291360
Karthik Burra <kburra193@gmail.com>
2022-08-19 15:29:46 -0500

    Sliders Fix and Code Cleanup

--- K&Etemplate.js
+++ K&Etemplate.js
@@ -1,39 +1,29 @@
-var prefix = window.location.pathname.substr(0,window.location.pathname.toLowerCase().lastIndexOf("/extensions") + 1);
-
+var prefix = window.location.pathname.substr(
+  0,
+  window.location.pathname.toLowerCase().lastIndexOf("/extensions") + 1
+);
 var config = {
   host: window.location.hostname,
   prefix: prefix,
   port: window.location.port,
-  isSecure: window.location.protocol === "https:"
+  isSecure: window.location.protocol === "https:",
 };
-
 require.config({
   baseUrl:
     (config.isSecure ? "https://" : "http://") +
     config.host +
     (config.port ? ":" + config.port : "") +
     config.prefix +
-    "resources"
+    "resources",
 });
-
-require(["js/qlik"], function(qlik) {
-  qlik.setOnError(function(error) {
+require(["js/qlik"], function (qlik) {
+  qlik.setOnError(function (error) {
     $("#popupText").append(error.message + "<br>");
     $("#popup").fadeIn(1000);
   });
-  $("#closePopup").click(function() {
+  $("#closePopup").click(function () {
     $("#popup").hide();
   });
-	
   //open apps -- inserted here --
-	
   //get objects -- inserted here --
-	
-	
-
-	
-
-
 });
-    
-    
