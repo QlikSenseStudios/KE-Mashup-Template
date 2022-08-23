075952ece662c45b2da6c238e8760d006624cd5c
Karthik Burra <kburra193@gmail.com>
2022-08-23 16:06:51 -0500

    Bookmarks,undo,forward,filters functionality part1

--- K&Etemplate.js
+++ K&Etemplate.js
@@ -16,6 +16,7 @@ require.config({
     config.prefix +
     "resources",
 });
+//Requaire Qlik APIs
 require(["js/qlik"], function (qlik) {
   qlik.setOnError(function (error) {
     $("#popupText").append(error.message + "<br>");
@@ -26,4 +27,70 @@ require(["js/qlik"], function (qlik) {
   });
   //open apps -- inserted here --
   //get objects -- inserted here --
+  //Grab Current Selections
+  app.getList("SelectionObject", function (reply) {
+    $selections = $("#currSelections");
+    $selections.html("");
+    //Setting Starting variable
+    var selectionsObject = reply.qSelectionObject.qSelections;
+    //console.log(selectionsObject);
+    //Total Selections Badge
+    var initialValue = 0;
+    var totalSelections = selectionsObject.reduce(function (acc, cur) {
+      return acc + cur.qSelectedCount;
+    }, initialValue);
+    if (totalSelections == 0) {
+      $(".notification .badge").hide();
+    } else {
+      $(".notification .badge").show().html(totalSelections);
+    }
+    //Loop through selections and append to modal
+    $.each(selectionsObject, function (key, value) {
+      //Setting Starting variables
+      var field = value.qField;
+      var numSelected = value.qSelectedCount;
+      var total = value.qTotal;
+      var threshold = 5;
+      var selectedStr = value.qSelected;
+      if (numSelected <= threshold) {
+        var html = "";
+        html += "<span class='selected-field-container' id='" + field + "'>";
+        html += "<span class='selected-field'>" + field + ": </span>";
+        html += selectedStr;
+        html += "<span class='clear-field'>X</span>";
+        html += "</span>";
+        $selections.append(html);
+      } else {
+        var html = "";
+        html += "<span class='selected-field-container' id='" + field + "'>";
+        html += "<span class='selected-field'>" + field + ": </span>";
+        html += numSelected + " of " + total;
+        html += "<span class='clear-field'>X</span>";
+        html += "</span>";
+        $selections.append(html);
+      }
+    });
+    //Clear selection
+    $(".clear-field").click(function () {
+      var field = $(this).parent().attr("id");
+      app.field(field).clear();
+    });
+  });
+  //selections Navigation
+  $("[data-control]").click(function () {
+    var $element = $(this);
+    switch (
+      $element.data("control") ////we are targeting that element using jquery
+    ) {
+      case "clear":
+        app.clearAll();
+        break;
+      case "back":
+        app.back();
+        break;
+      case "forward":
+        app.forward();
+        break;
+    }
+  });
 });
