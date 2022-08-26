736ab5a91fc8d45275d36ebc77863dae1d3eac1b
Karthik Burra <kburra193@gmail.com>
2022-08-26 12:12:35 -0500

    Updates to Filter panel and Selections

--- K&Etemplate.js
+++ K&Etemplate.js
@@ -1,12 +1,9 @@
-var prefix = window.location.pathname.substr(
-  0,
-  window.location.pathname.toLowerCase().lastIndexOf("/extensions") + 1
-);
+var prefix = window.location.pathname.substr(0,window.location.pathname.toLowerCase().lastIndexOf("/extensions") + 1);
 var config = {
   host: window.location.hostname,
   prefix: prefix,
   port: window.location.port,
-  isSecure: window.location.protocol === "https:",
+  isSecure: window.location.protocol === "https:"
 };
 require.config({
   baseUrl:
@@ -14,43 +11,50 @@ require.config({
     config.host +
     (config.port ? ":" + config.port : "") +
     config.prefix +
-    "resources",
+    "resources"
 });
 //Requaire Qlik APIs
-require(["js/qlik"], function (qlik) {
-  qlik.setOnError(function (error) {
+require(["js/qlik"], function(qlik) {
+  qlik.setOnError(function(error) {
     $("#popupText").append(error.message + "<br>");
     $("#popup").fadeIn(1000);
   });
-  $("#closePopup").click(function () {
+  $("#closePopup").click(function() {
     $("#popup").hide();
   });
+	
   //open apps -- inserted here --
+	
   //get objects -- inserted here --
-  //Grab Current Selections
-  app.getList("SelectionObject", function (reply) {
+	
+	
+	//Grab Current Selections
+  app.getList("SelectionObject", function(reply) {
     $selections = $("#currSelections");
     $selections.html("");
     //Setting Starting variable
     var selectionsObject = reply.qSelectionObject.qSelections;
-    //console.log(selectionsObject);
+    console.log(reply.qSelectionObject);
+	console.log(reply.qSelectionObject.qSelections);
+	 var selectionsObject1 = reply.qSelectionObject.qSelections.length;
+	console.log(reply.qSelectionObject.qSelections.length);
     //Total Selections Badge
     var initialValue = 0;
-    var totalSelections = selectionsObject.reduce(function (acc, cur) {
+    var totalSelections = selectionsObject.reduce(function(acc, cur) {
       return acc + cur.qSelectedCount;
     }, initialValue);
     if (totalSelections == 0) {
       $(".notification .badge").hide();
     } else {
-      $(".notification .badge").show().html(totalSelections);
+      $(".notification .badge").show().html(selectionsObject1);
     }
     //Loop through selections and append to modal
-    $.each(selectionsObject, function (key, value) {
+    $.each(selectionsObject, function(key, value) {
       //Setting Starting variables
       var field = value.qField;
       var numSelected = value.qSelectedCount;
       var total = value.qTotal;
-      var threshold = 5;
+      var threshold = 3;
       var selectedStr = value.qSelected;
       if (numSelected <= threshold) {
         var html = "";
@@ -71,26 +75,51 @@ require(["js/qlik"], function (qlik) {
       }
     });
     //Clear selection
-    $(".clear-field").click(function () {
-      var field = $(this).parent().attr("id");
+    $(".clear-field").click(function() {
+      var field = $(this)
+        .parent()
+        .attr("id");
       app.field(field).clear();
     });
   });
-  //selections Navigation
-  $("[data-control]").click(function () {
-    var $element = $(this);
-    switch (
-      $element.data("control") ////we are targeting that element using jquery
-    ) {
-      case "clear":
-        app.clearAll();
-        break;
-      case "back":
-        app.back();
-        break;
-      case "forward":
-        app.forward();
-        break;
-    }
-  });
+	//selections Navigation
+	$('[data-control]').click(function(){
+		var $element = $(this);
+		switch ($element.data('control')) {                    ////we are targeting that element using jquery
+			case 'clear':
+				app.clearAll()
+			
+			break;
+			case 'back':
+				app.back()
+			break;
+			
+			
+			
+			case 'forward':
+				app.forward()
+			break;
+			
+		}
+	});
+	
+	
+	// find the bootstrap tab changing event
+	// invoke qlik.resize(); in it
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 });
+    
+    
